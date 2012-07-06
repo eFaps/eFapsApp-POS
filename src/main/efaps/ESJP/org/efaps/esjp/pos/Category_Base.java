1ca91c1c2fcb27f6ac1216f63036943a8ea5fbe4
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2012-07-06 15:27:36 +0000

    - adding update category trigger
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/pos/trunk@7752 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/pos/Category_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/pos/Category_Base.java
@@ -21,16 +21,13 @@
 package org.efaps.esjp.pos;
 
 import java.io.StringWriter;
-import java.util.Map;
 import java.util.UUID;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 
-import org.efaps.admin.datamodel.Attribute;
 import org.efaps.admin.event.Parameter;
-import org.efaps.admin.event.Parameter.ParameterValues;
 import org.efaps.admin.event.Return;
 import org.efaps.admin.program.esjp.EFapsRevision;
 import org.efaps.admin.program.esjp.EFapsUUID;
@@ -66,13 +63,13 @@ public abstract class Category_Base
         print.execute();
         final String catOid = print.<String>getSelect(catOidSel);
         final Instance catInst = Instance.get(catOid);
-        sendMsg(_parameter, catInst, CIPOS.MessageCategoryConnect.uuid);
+        enque4SendMsg(_parameter, catInst, CIPOS.MessageCategoryConnect.uuid);
         return ret;
     }
 
-    protected void sendMsg(final Parameter _parameter,
-                           final Instance _catInst,
-                           final UUID _messageTypeUUID)
+    protected void enque4SendMsg(final Parameter _parameter,
+                                 final Instance _catInst,
+                                 final UUID _messageTypeUUID)
         throws EFapsException
     {
 
@@ -123,52 +120,7 @@ public abstract class Category_Base
     public Return updateTrigger(final Parameter _parameter)
         throws EFapsException
     {
-        final Return ret = new Return();
-        final Map<?, ?> values = (Map<?, ?>) _parameter.get(ParameterValues.NEW_VALUES);
-
-        final Attribute uuidAttr = CIPOS.Category.getType().getAttribute(CIPOS.Category.UUID.name);
-        final Attribute nameAttr = CIPOS.Category.getType().getAttribute(CIPOS.Category.Name.name);
-        final Attribute parentAttr = CIPOS.Category.getType().getAttribute(CIPOS.Category.ParentLink.name);
-        final CategoryInfo cat = new CategoryInfo();
-        cat.setName((String) ((Object[]) values.get(nameAttr))[0]);
-        cat.setUuid((String) ((Object[]) values.get(uuidAttr))[0]);
-        if (values.containsKey(parentAttr)) {
-
-        }
-
-        try {
-            final JAXBContext jc = JAXBContext.newInstance(CategoryInfo.class);
-            final Marshaller marschaller = jc.createMarshaller();
-            final StringWriter writer = new StringWriter();
-            marschaller.marshal(cat, writer);
-
-            final Instance inst = _parameter.getInstance();
-            final QueryBuilder queryBldr = new QueryBuilder(CIPOS.Category2SubscriptionCategory);
-            queryBldr.addWhereAttrEqValue(CIPOS.Category2SubscriptionCategory.FromLink, inst.getId());
-            final MultiPrintQuery multi = queryBldr.getPrint();
-            final SelectBuilder jmsOidSel = new SelectBuilder().linkto(CIPOS.Category2SubscriptionCategory.ToLink)
-                            .linkto(CIPOS.SubscriptionCategory.JmsLink).oid();
-            multi.addSelect(jmsOidSel);
-            multi.execute();
-            while (multi.next()) {
-                final String jmsOid = multi.<String>getSelect(jmsOidSel);
-                final Instance jmsInst = Instance.get(jmsOid);
-                if (jmsInst.isValid()) {
-                    // final Insert insert = new
-                    // Insert(CIPOS.MessageCategoryInsert);
-                    // insert.add(CIPOS.MessageCategoryInsert.Content,
-                    // writer.toString());
-                    // insert.add(CIPOS.MessageCategoryInsert.JmsLink,
-                    // jmsInst.getId());
-                    // insert.execute();
-                }
-            }
-
-        } catch (final JAXBException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-        return ret;
+        enque4SendMsg(_parameter, _parameter.getInstance(), CIPOS.MessageCategoryUpdate.uuid);
+        return new Return();
     }
 }
