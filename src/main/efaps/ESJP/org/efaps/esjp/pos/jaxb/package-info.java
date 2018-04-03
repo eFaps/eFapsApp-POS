c357f003bea256043257aa3e324a607399562778
Jan Moxter <jan.moxter@innobix.com>
2018-04-02 21:03:59 -0500

    - remove jms and subscription

--- src/main/efaps/ESJP/org/efaps/esjp/pos/jaxb/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * The classes of this package are used to generate and read xml using
- * jaxb. To be able to sync it with the pos4a project.
- *
- * @author The eFaps Team
- * @version $Id$
- */
-package org.efaps.esjp.pos.jaxb;
--- src/main/efaps/ESJP/org/efaps/esjp/pos/jms/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * TODO comment!
- *
- * @author The eFaps Team
- * @version $Id$
- */
-package org.efaps.esjp.pos.jms;
