47c2072540682655ea3e39b3ce729997d34ae3a9
Jan Moxter <jan.moxter@innobix.com>
2018-06-10 21:51:52 -0500

    Adding REST endpoint for warehouses

---
+++ src/main/efaps/ESJP/org/efaps/esjp/pos/rest/Warehouse.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2003 - 2018 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.efaps.esjp.pos.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.efaps.admin.program.esjp.EFapsApplication;
+import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.util.EFapsException;
+
+@EFapsUUID("af40729e-51f2-4b78-8caf-51fd7095dad0")
+@EFapsApplication("eFapsApp-POS")
+@Path("/pos")
+public class Warehouse
+    extends Warehouse_Base
+{
+    @Override
+    @Path("warehouses")
+    @GET
+    @Produces({ MediaType.APPLICATION_JSON })
+    public Response getWarehouses()
+        throws EFapsException
+    {
+        return super.getWarehouses();
+    }
+}
---
+++ src/main/efaps/ESJP/org/efaps/esjp/pos/rest/Warehouse_Base.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2003 - 2018 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.efaps.esjp.pos.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.core.Response;
+
+import org.efaps.admin.datamodel.Status;
+import org.efaps.admin.program.esjp.EFapsApplication;
+import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.db.MultiPrintQuery;
+import org.efaps.db.QueryBuilder;
+import org.efaps.esjp.ci.CIProducts;
+import org.efaps.pos.dto.WarehouseDto;
+import org.efaps.util.EFapsException;
+
+@EFapsUUID("5304130a-0f0b-4320-84c6-25a3d63f6a6e")
+@EFapsApplication("eFapsApp-POS")
+public abstract class Warehouse_Base
+{
+
+    /**
+     * Gets the warehouses.
+     *
+     * @return the warehouses
+     * @throws EFapsException the eFaps exception
+     */
+    public Response getWarehouses()
+        throws EFapsException
+    {
+        final List<WarehouseDto> warehouses = new ArrayList<>();
+        final QueryBuilder queryBldr = new QueryBuilder(CIProducts.Warehouse);
+        queryBldr.addWhereAttrEqValue(CIProducts.Warehouse.Status, Status.find(
+                        CIProducts.StorageAbstractStatus.Active));
+        final MultiPrintQuery multi = queryBldr.getPrint();
+        multi.addAttribute(CIProducts.Warehouse.Name);
+        multi.execute();
+        while (multi.next()) {
+            warehouses.add(WarehouseDto.builder()
+                .withOID(multi.getCurrentInstance().getOid())
+                .withName(multi.getAttribute(CIProducts.Warehouse.Name))
+                .build());
+        }
+        final Response ret = Response.ok().entity(warehouses).build();
+        return ret;
+    }
+}
