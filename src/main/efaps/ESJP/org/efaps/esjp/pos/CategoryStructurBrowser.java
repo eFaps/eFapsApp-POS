44a9bd586c19060250f0df77304331da311d989a
Jan Moxter <jan@moxter.net>
2022-08-28 22:07:56 -0500

    Sub-Categories

---
+++ src/main/efaps/ESJP/org/efaps/esjp/pos/CategoryStructurBrowser.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2003 - 2022 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.efaps.esjp.pos;
+
+import org.efaps.admin.program.esjp.EFapsApplication;
+import org.efaps.admin.program.esjp.EFapsUUID;
+
+@EFapsUUID("5d43c497-04d6-4fab-85ae-765fb8659fe7")
+@EFapsApplication("eFapsApp-POS")
+public class CategoryStructurBrowser
+    extends CategoryStructurBrowser_Base
+{
+
+}
---
+++ src/main/efaps/ESJP/org/efaps/esjp/pos/CategoryStructurBrowser_Base.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2003 - 2022 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.efaps.esjp.pos;
+
+import org.efaps.admin.event.Parameter;
+import org.efaps.admin.event.Return;
+import org.efaps.admin.program.esjp.EFapsApplication;
+import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.eql.builder.Where;
+import org.efaps.esjp.ci.CIPOS;
+import org.efaps.esjp.ui.structurbrowser.StandartStructurBrowser;
+import org.efaps.util.EFapsException;
+/**
+ * TODO description!
+ *
+ * @author The eFasp Team
+ */
+@EFapsUUID("cc86a55a-93c3-4cfa-a008-e37ecaa9c3c4")
+@EFapsApplication("eFapsApp-POS")
+public abstract class CategoryStructurBrowser_Base
+    extends StandartStructurBrowser
+{
+
+    @Override
+    protected Return checkHideColumn4Row(final Parameter _parameter)
+        throws EFapsException
+    {
+        return new Return();
+    }
+
+    @Override
+    protected void add2MainQuery(final Parameter _parameter, final Where _where)
+        throws EFapsException
+    {
+       _where.attribute(CIPOS.Category.ParentLink).isNull();
+    }
+
+}
