f350981885e136044b3f628bbe0f88685096fe50
Jan Moxter <jan@moxter.net>
2024-02-02 19:50:20 -0500

    Headers

--- 2_BE.sql
+++ 2_BE.sql
@@ -1,1 +1,17 @@
+--
+-- Copyright © 2003 - 2024 The eFaps Team (-)
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
 UPDATE T_POSBE set TYPEID = (select ID from t_cmabstract where uuid='7428ea5f-02dd-489d-978e-26b2cf15fe51');
