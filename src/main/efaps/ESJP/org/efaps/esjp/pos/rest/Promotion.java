0ec8926877c78b761b1bba452450b2d760e780e1
Jan Moxter <jan@moxter.net>
2024-06-20 20:51:29 -0500

    Sync promoInfo

--- src/main/efaps/ESJP/org/efaps/esjp/pos/rest/Promotion.java
+++ src/main/efaps/ESJP/org/efaps/esjp/pos/rest/Promotion.java
@@ -16,6 +16,7 @@
 package org.efaps.esjp.pos.rest;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -24,7 +25,11 @@ import javax.ws.rs.core.Response;
 
 import org.efaps.admin.program.esjp.EFapsApplication;
 import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.esjp.db.InstanceUtils;
 import org.efaps.esjp.promotions.PromotionService;
+import org.efaps.pos.dto.PromoInfoSyncDto;
+import org.efaps.promotionengine.dto.PromotionDetailDto;
+import org.efaps.promotionengine.dto.PromotionInfoDto;
 import org.efaps.util.EFapsException;
 
 @EFapsUUID("a3abf58f-11b0-4b2f-a5c8-cf69bf897b41")
@@ -33,6 +38,7 @@ import org.efaps.util.EFapsException;
 public class Promotion
     extends AbstractRest
 {
+
     @Path("/{identifier}/promotions")
     @GET
     @Produces({ MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN })
@@ -43,6 +49,38 @@ public class Promotion
         final var promotions = new PromotionService().getPromotions();
         final Response ret = Response.ok()
                         .entity(promotions)
+                        .build();
+        return ret;
+    }
+
+    @Path("/{identifier}/promotion-infos")
+    @POST
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN })
+    public Response registerPromotionInfo(@PathParam("identifier") final String identifier,
+                                          final PromoInfoSyncDto dto)
+        throws EFapsException
+    {
+        checkAccess(identifier, ACCESSROLE.BE);
+        final var promoInfo = dto.getPromoInfo();
+        final var promotionInfoDto = PromotionInfoDto.builder()
+                        .withNetTotalDiscount(promoInfo.getNetTotalDiscount())
+                        .withCrossTotalDiscount(promoInfo.getCrossTotalDiscount())
+                        .withPromotionOids(promoInfo.getPromotionOids())
+                        .withDetails(promoInfo.getDetails().stream()
+                                        .map(promoDetail -> PromotionDetailDto.builder()
+                                                        .withNetUnitDiscount(promoDetail.getNetUnitDiscount())
+                                                        .withNetDiscount(promoDetail.getNetDiscount())
+                                                        .withCrossUnitDiscount(promoDetail.getCrossUnitDiscount())
+                                                        .withCrossDiscount(promoDetail.getCrossDiscount())
+                                                        .withIndex(promoDetail.getIndex())
+                                                        .withPromotionOid(promoDetail.getPromotionOid())
+                                                        .build())
+                                        .toList())
+                        .build();
+        final var instance = new PromotionService().registerPromotionInfoForDoc(dto.getDocumentOid(), promotionInfoDto,
+                        dto.getPromotions());
+        final Response ret = Response.ok()
+                        .entity(InstanceUtils.isValid(instance) ? instance.getOid() : instance)
                         .build();
         return ret;
     }
