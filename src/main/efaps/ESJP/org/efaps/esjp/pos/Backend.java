00d8cc8f1702482eea2d393f2365155473f866f9
Jan Moxter <jan.moxter@moxter.net>
2026-03-11 16:55:35 -0500

    Update Template

--- src/main/efaps/ESJP/org/efaps/esjp/pos/Backend.java
+++ src/main/efaps/ESJP/org/efaps/esjp/pos/Backend.java
@@ -15,11 +15,20 @@
  */
 package org.efaps.esjp.pos;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 import org.efaps.admin.event.Parameter;
+import org.efaps.admin.event.Parameter.ParameterValues;
 import org.efaps.admin.event.Return;
+import org.efaps.admin.event.Return.ReturnValues;
 import org.efaps.admin.program.esjp.EFapsApplication;
 import org.efaps.admin.program.esjp.EFapsUUID;
 import org.efaps.db.Insert;
+import org.efaps.eql.EQL;
 import org.efaps.esjp.ci.CIPOS;
 import org.efaps.esjp.common.uiform.Create;
 import org.efaps.util.EFapsException;
@@ -64,4 +73,35 @@ public class Backend
         }.execute(parameter);
     }
 
+    public Return autoComplete4Backend(final Parameter parameter)
+        throws EFapsException
+    {
+        final String input = (String) parameter.get(ParameterValues.OTHERS);
+        final List<Map<String, String>> list = new ArrayList<>();
+        if (input.length() > 0) {
+            final Map<String, Map<String, String>> tmpMap = new TreeMap<>();
+
+            final var eval = EQL.builder().print().query(CIPOS.Backend)
+                            .where()
+                            .attribute(CIPOS.Backend.Name).ilike(input)
+                            .select().attribute(CIPOS.Backend.Name, CIPOS.Backend.Description)
+                            .evaluate();
+
+            while (eval.next()) {
+                final long id = eval.inst().getId();
+                final String name = eval.get(CIPOS.Backend.Name);
+                final String description = eval.get(CIPOS.Backend.Description);
+                final String choice = name + " - " + description;
+                final Map<String, String> map = new HashMap<>();
+                map.put("eFapsAutoCompleteKEY", String.valueOf(id));
+                map.put("eFapsAutoCompleteCHOICE", choice);
+                map.put("eFapsAutoCompleteVALUE", name);
+                tmpMap.put(choice, map);
+            }
+            list.addAll(tmpMap.values());
+        }
+        final Return retVal = new Return();
+        retVal.put(ReturnValues.VALUES, list);
+        return retVal;
+    }
 }
